b98077ac8cc266652ff2963af8180cd36f187f87
fatemeh-ra <fatemeh.r3366@gmail.com>
2021-08-17 03:03:02 +0430

    Setting updated

--- raffle-back-end/app/helpers/Configs.scala
+++ raffle-back-end/app/helpers/Configs.scala
@@ -9,18 +9,11 @@ object Configs extends ConfigHelper {
   lazy val nodeApiKey: String = readKey("node.apiKey", "")
   lazy val networkType: NetworkType = if (readKey("node.networkType").toLowerCase.equals("mainnet")) NetworkType.MAINNET else NetworkType.TESTNET
   lazy val addressEncoder = new ErgoAddressEncoder(networkType.networkPrefix)
-
   lazy val explorerUrl: String = readKey("explorer.url")
-  lazy val defaultTxFee: Long =  1000000L
 
   lazy val fee: Long = readKey("fee").toLong
-  lazy val dummyTxId: String = readKey("dummyTxId")
   lazy val serviceSecret: BigInteger = BigInt(readKey("service.secret"), 16).bigInteger
   lazy val serviceAddress: Address = Address.create(readKey("service.address"))
-  lazy val serviceTokenId: String = readKey("service.token")
-  lazy val raffleProjectAddress: Address = Address.create(readKey("raffle.project.address"))
-  lazy val oracleId: String = readKey("oracleId")
-  lazy val servicePercent: Long = readKey("service.percent").toLong
 
   lazy val creationDelay: Int = readKey("creationDelay").toInt
   lazy val checkingDelay: Int = readKey("checkingDelay").toInt
@@ -30,8 +23,6 @@ object Configs extends ConfigHelper {
   lazy val creationThreadInterval: Int = readKey("creationThreadInterval").toInt
   lazy val donateThreadInterval: Int = readKey("donateThreadInterval").toInt
   lazy val refundThreadInterval: Int = readKey("refundThreadInterval").toInt
-  lazy val raffleThreadInterval: Int = readKey("raffleThreadInterval").toInt
-  lazy val searchThreadInterval: Int = readKey("searchThreadInterval").toInt
 
   lazy val recaptchaKey: String = readKey("recaptchaKey")
 
